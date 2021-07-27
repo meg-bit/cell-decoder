4538ca7477535712291026c2f09d47d03df96dfd
meg-bit <margaretpaiva20@gmail.com>
2021-07-27 17:11:55 +0000

    img_utils.py has check_shape, view_image, and revove_border functions so far

---
+++ DeCRISP/img_utils.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+def check_shape(X):
+    return X.shape
+
+def view_img(X):
+    for i in range(X.shape[0]):
+    plt.figure(dpi=250)
+    plt.imshow(X[i, 0,])
+    
+def remove_border(X, up, down, left, right):
+    return X[:, :, up:down, left:right]
+
+# def subtract_background(X, window_size):
