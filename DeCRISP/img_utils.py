f7786cbd68943daf80b4c05f1214379343d62e60
meg-bit <margaretpaiva20@gmail.com>
2021-07-28 20:26:43 +0000

    Correct indentation errors in img_utils.py and utils.py

--- DeCRISP/img_utils.py
+++ DeCRISP/img_utils.py
@@ -5,8 +5,8 @@ def check_shape(X):
 
 def view_img(X):
     for i in range(X.shape[0]):
-    plt.figure(dpi=250)
-    plt.imshow(X[i, 0,])
+        plt.figure(dpi=250)
+        plt.imshow(X[i, 0,])
     
 def remove_border(X, up, down, left, right):
     return X[:, :, up:down, left:right]
